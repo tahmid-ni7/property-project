7fed9f26d41071e2cf12f3a78aab6ee4ab843c9d
Tahmid Nishat <tahmid.ni7@gmail.com>
2020-02-05 12:02:01 +0600

    Home page and about page templating

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from pages import views
 
+app_name = 'pages'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('about/', views.about, name='about'),
 ]
