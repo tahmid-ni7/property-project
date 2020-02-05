56ce42081f11e032c5d6acae73fdbd68579b384f
Tahmid Nishat <tahmid.ni7@gmail.com>
2020-02-05 16:35:30 +0600

    'Listings-templating'

---
+++ listings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from listings import views
+
+app_name = 'listings'
+
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:listing_id>', views.listing, name='listing'),
+    path('search', views.search, name='search'),
+]
--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,5 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('about', views.about, name='about'),
 ]
