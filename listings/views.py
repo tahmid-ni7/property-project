98af8d9f5eccdf2d44d7ac342d81cf0cbccc11dd
Tahmid Nishat <tahmid.ni7@gmail.com>
2020-02-06 14:30:52 +0600

    'all-listing-shows-at-front-end-and-make-pagination'

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from .models import Listing
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
+    context = {'listings': paged_listings}
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listings/single_listing.html')
 
 
