83da7d8039ea37e9e690cae48cd13e866964779d
Tahmid Nishat <tahmid.ni7@gmail.com>
2020-02-09 18:59:13 +0600

    'login-both-username-and-email'

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render,redirect
-from django.contrib import messages,auth
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from contacts.models import Contact
+
 
 # Create your views here.
 def register(request):
@@ -27,8 +28,8 @@ def register(request):
                 return redirect('accounts:register')
             else:
                 # that's fine
-                user = User.objects.create_user(username=username,password=password, email=email,
-                                                first_name=first_name,last_name=last_name)
+                user = User.objects.create_user(username=username, password=password, email=email,
+                                                first_name=first_name, last_name=last_name)
                 # login after register
                 # auth.login(request, user)
                 # messages.success(request, 'You are logged in now')
@@ -43,15 +44,20 @@ def register(request):
     else:
         return render(request, 'accounts/register.html')
 
+
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
-        user = auth.authenticate(username=username, password=password)
+        try:
+            user = auth.authenticate(username=User.objects.get(email=username), password=password)
+
+        except:
+            user = auth.authenticate(username=username, password=password)
 
         if user is not None:
-            auth.login(request,user)
+            auth.login(request, user)
             messages.success(request, "You are now logged in")
             return redirect('accounts:dashboard')
         else:
@@ -59,6 +65,7 @@ def login(request):
             return redirect('accounts:login')
     else:
         return render(request, 'accounts/login.html')
+
 
 @login_required()
 def logout(request):
